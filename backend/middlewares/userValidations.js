fb4a62a64e08afd4aacbb5c0fd90e13a6f3caaa9
Michael <michaelpetterson06.com.br>
2023-05-26 19:29:11 -0300

    Validações de criação de usuário

---
+++ backend/middlewares/userValidations.js
@@ -0,0 +1,25 @@
+const { body } = require('express-validator');
+
+const userCreateValidation = () => {
+    return [ body('name').isString().withMessage('O nome é obrigatório')
+                .isLength({min: 3}).withMessage('O nome precisa ter no mínimo 3 caracteres.'),
+             body('email').isString().withMessage('O email é obrigatório.')
+                .isEmail().withMessage('Insira um email valido.'),
+             body('password').isString()
+                .withMessage('A senha é obrigatória.')
+                .isLength({min: 5}).withMessage('A senha precisa ter no mínimo 5 caractreres'),
+             body('confirmPassword').isString()
+                                    .withMessage('A confirmação de senha é obrigatória.')
+             .custom((value,{req}) => {
+                if(value != req.body.password) {
+                    throw new Error('As senhas não são iguais.');
+                }
+                return true;
+             })   
+           ];
+}
+
+
+module.exports = {
+    userCreateValidation,
+}
