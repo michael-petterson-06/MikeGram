b3861440cca116f6c777e7b13cd3d89832b57abd
Michael <michaelpetterson06.com.br>
2023-05-27 20:27:07 -0300

    Middleware de upload de imagens

--- backend/middlewares/authGuard.js
+++ backend/middlewares/authGuard.js
@@ -4,8 +4,7 @@ const jwtSecret = process.env.JWT_SECRET;
 
 
 const authGuard = async (req, res, next ) => {
-
-    console.log('estou ak')
+    
     const authHeader = req.headers['authorization'];
     
     const token = authHeader && authHeader.split(' ')[1];
