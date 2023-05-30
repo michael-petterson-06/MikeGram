24a31c2e7b7a50fb7613f6ad09afc9af1674322e
Michael <michaelpetterson06.com.br>
2023-05-29 21:30:18 -0300

    Excluíndo fotos

--- backend/controllers/PhotoController.js
+++ backend/controllers/PhotoController.js
@@ -7,7 +7,6 @@ const mongoose = require('mongoose');
 const insertPhoto = async (req, res) => {
     const { title } = req.body;
     const  image  = req.file.filename;
-    
     const reqUser = req.user;
     const user = await User.findById(reqUser._id);
 
@@ -24,11 +23,35 @@ const insertPhoto = async (req, res) => {
         res.status(422).json({
             errors: ['Houve um problema tente novamente mais tarde.']
         });
+        return;
     }
-    
     res.status(201).json(newPhoto);
 }
 
-module.exports = { insertPhoto };
+// Remover foto
+const deletePhoto = async(req,res) => {
+    const { id } = req.params;
+    try {
+        const reqUser = req.user;
+        const photo = await Photo.findById(new mongoose.Types.ObjectId(id));
+        if(!photo) {
+            res.status(404).json({ errors: ['Foto não encontrada']});
+        return;
+    }
+    // Checar se foto pertence ao usuário
+    if(!photo.userId.equals(reqUser._id)) {
+        res.status(422).json({ errors: ['Ocorreu um erro por favor tente mais tarde.']});
+    }
+
+    await Photo.findByIdAndDelete(photo._id);
+
+    res.status(200).json({ id: photo._id, message: 'Foto excluída com sucesso.'});
+    } catch (error) {
+        res.status(404).json({ errors: ['Foto não encontrada']});
+        return;
+    }
+}   
+
+module.exports = { insertPhoto, deletePhoto };
 
 
