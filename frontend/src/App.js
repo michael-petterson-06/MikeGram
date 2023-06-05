b9ed0d82ab44a70e5a51b798fe01dc24d29e7077
Michael <michaelpetterson06.com.br>
2023-06-05 11:31:21 -0300

    Criando componente de navbar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,16 +6,23 @@ import Home from './pages/Home/Home';
 import Register from './pages/Auth/Register';
 import Login from './pages/Auth/Login';
 
+//Componentes
+import NavBar from './components/navbar/NavBar';
+import Footer from './components/footer/Footer';
+
+
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
+        <NavBar/>
         <Routes>
           <Route path='/' element={<Home/>}/>
           <Route path='/register' element={<Register/>}/>
           <Route path='/login' element={<Login/>} />
         </Routes>
+        <Footer/>
       </BrowserRouter>
     </div>
   );
---
+++ frontend/src/components/footer/Footer.js
@@ -0,0 +1,13 @@
+import './Footer.css';
+
+const Footer = () => {
+    return (
+        <footer id='footer'>
+            <p>ReactGram &copy; 2023</p>
+            <p>Michael Petterson</p>
+        </footer>
+    )
+}
+
+
+export default Footer;
