7d13ee4afb3ff716774a6a4e5576bde8115caa21
Michael <michaelpetterson06.com.br>
2023-06-06 20:18:23 -0300

    Criando reducer de usuário

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,9 +1,11 @@
 import {configureStore} from '@reduxjs/toolkit';
 
 import authReducer from './slices/AuthSlice';
+import userReducer from './slices/userSlice';
 
 export const store = configureStore({
     reducer:{
         auth: authReducer,
+        user: userReducer,
     },
 });
