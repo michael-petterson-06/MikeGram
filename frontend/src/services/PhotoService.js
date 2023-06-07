c91301c43c67417b2196d154ed1095a5b597cd12
Michael <michaelpetterson06.com.br>
2023-06-07 17:05:09 -0300

    getUserPhotos

--- frontend/src/services/PhotoService.js
+++ frontend/src/services/PhotoService.js
@@ -3,25 +3,34 @@ import { api, requestConfig } from '../utils/config';
 
 //Publicando uma foto
 const publishPhoto = async(data, token) => {
-
     const config = requestConfig('POST', data, token, true);
-    
-
     try {
         const res = await fetch(api + '/photos', config)
                         .then((res) => res.json())
                         .catch((error) => error)
-                        
-
-        return res;                            
+       return res;                            
     } catch(error){
         console.log(error);
     }
-
 }
+
+//Resgatando fotos de um usuário específico.
+const getUserPhotos = async(id) => {
+    const config = requestConfig('GET');
+    try {
+        const res = await fetch(api + '/photos/user/'+id, config)
+                            .then((res) => res.json())
+                            .catch((error) => error)
+        return res;                               
+    } catch(err) {
+        console.log(err);
+    }
+}
+
 
 const photoService = {
     publishPhoto,
+    getUserPhotos,
 };
 
 export default photoService;
