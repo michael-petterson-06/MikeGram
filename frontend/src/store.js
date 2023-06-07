4bcaf224c8391f415e340d5cd4f577b44dc4c7cb
Michael <michaelpetterson06.com.br>
2023-06-07 15:30:02 -0300

    Iniciando reducer de fotos

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -2,10 +2,12 @@ import {configureStore} from '@reduxjs/toolkit';
 
 import authReducer from './slices/AuthSlice';
 import userReducer from './slices/userSlice';
+import photoReducer from './slices/PhotoSlice'
 
 export const store = configureStore({
     reducer:{
         auth: authReducer,
         user: userReducer,
+        photoReducer,
     },
 });
