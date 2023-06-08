88e13d60b7eb0cd0467710f46ac5abbd9f2a8ade
Michael <michaelpetterson06.com.br>
2023-06-07 21:24:01 -0300

    Service de exclusão de fotos

--- frontend/src/services/PhotoService.js
+++ frontend/src/services/PhotoService.js
@@ -21,10 +21,23 @@ const getUserPhotos = async(id, token) => {
         const res = await fetch(api + '/photos/user/'+id, config)
                             .then((res) => res.json())
                             .catch((error) => error)
-                            console.log(res)
         return res;                               
     } catch(err) {
         console.log(err);
+    }
+}
+
+//Deletar foto
+const deletePhoto = async(id, token) => {
+    const config = requestConfig('DELETE', null, token);
+
+    try {
+        const res = await fetch(api + '/photos/' + id, config)
+                        .then((res) => res.json())
+                        .catch((err) => err)
+        return res;                        
+    } catch (error) {
+        console.log(error)
     }
 }
 
@@ -32,6 +45,7 @@ const getUserPhotos = async(id, token) => {
 const photoService = {
     publishPhoto,
     getUserPhotos,
+    deletePhoto,
 };
 
 export default photoService;
