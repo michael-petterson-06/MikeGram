99f529e3f9fb813d08fa75f20ac158b525c339be
Michael <michaelpetterson06.com.br>
2023-06-07 23:43:24 -0300

    Service da foto pelo id

--- frontend/src/pages/photo/Photo.js
+++ frontend/src/pages/photo/Photo.js
@@ -1,5 +1,14 @@
 import React from 'react';
 import { uploads } from "../../utils/config";
+// Componentes
+// import Message from "../../components/Message";
+import { Link } from "react-router-dom";
+// Hooks
+import { useEffect, useState } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { useParams } from "react-router-dom";
+
+
 
 const Photo = () => {;
   return (
--- frontend/src/services/PhotoService.js
+++ frontend/src/services/PhotoService.js
@@ -54,11 +54,25 @@ const updatePhoto = async(data, id, token) => {
     };
 };
 
+//Foto pelo id
+const getPhoto =async(id) => {
+    const config = requestConfig("GET");
+    try {
+        const res = await fetch(api + '/photos/' + id, config)      
+                            .then((res)=> res.json())
+                            .catch((err) => err);
+        return res;                    
+    } catch (error) {
+        console.log(error);
+    };
+};
+
 const photoService = {
     publishPhoto,
     getUserPhotos,
     deletePhoto,
     updatePhoto,
+    getPhoto,
 };
 
 export default photoService;
